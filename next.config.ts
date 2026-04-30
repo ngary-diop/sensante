289e25a0c049c4d1054d3e9166b84e610a60c4f2
Aliou Loum <aliouloum2004@gmail.com>
2026-04-30 11:53:14 +0000

    Add Pilote IP to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-﻿import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.56.1', 'localhost'],
+  allowedDevOrigins: ['192.168.56.1', '192.168.100.1', 'localhost'],
   /* config options here */
 };
 
